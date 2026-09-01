687cfb2c9bf1d867cfe6068850b4dafecd6c42d8
pawanpreet singh <pawanpreet323199@gmail.com>
2026-09-01 15:30:19 +1200

    edit db.php

--- Index.php
+++ Index.php
@@ -1,1 +1,53 @@
+<?php
+require_once(__DIR__ . '/db.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <title>PS saw mill</title>
+ <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <!-- Navbar -->
+    <nav class="navbar">
+
+        <!-- Logo -->
+        <a href="index.html" class="logo">
+            PS
+        </a>
+
+        <!-- Brand -->
+        <div class="brand">
+          Pawan saw_mill
+        </div>
+      
+
+        <!-- Menu -->
+        <div class="nav-links">
+            <a href="Home">Home</a>
+            <a href="EXPLORE">EXPLORE</a>
+            <a href="ABOUT">ABOUT</a>
+            <a href="CONTACT US">CONTACT US</a>
+        </div>
+
+    </nav>
+    <!--footer---->
+    <Footer>
+<div class="container">
+    <ul class="flex_row">
+            <a href="Home">Home</a>
+            <a href="EXPLORE">EXPLORE</a>
+            <a href="ABOUT">ABOUT</a>
+            <a href="CONTACT US">CONTACT US</a>
+    </ul>
+</div>
+    </Footer>
+    
+    
+</body>
+</html>
